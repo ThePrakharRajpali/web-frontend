2e7db0ace415f88a916783517a90f74383e61a5b
Prakhar Rajpali <rajpaliprakhar23@gmail.com>
2021-10-22 23:31:38 +0530

    dropwdown done

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,6 +8,8 @@ import "../public/css/navbar.css";
 const Navbar = () => {
   const [colorChange, setColorChange] = useState(false);
   const [activeTab, setActiveTab] = useState("Home");
+  const [dropdown, setDropdown] = useState("none");
+
   const changeNavbarColor = () => {
     if (window.scrollY >= 80) {
       setColorChange(true);
@@ -20,53 +22,76 @@ const Navbar = () => {
     setActiveTab(data);
   };
 
+  const activateDropwdown = () => {
+    setDropdown("");
+  };
+  const deactivateDropdown = () => {
+    setDropdown("none");
+  };
   useEffect(() => {
     changeNavbarColor();
     window.addEventListener("scroll", changeNavbarColor);
   }, []);
 
   return (
-    <nav className={colorChange ? "solid-nav" : ""}>
-      <div className="nav-logo">
-        <img src={logo} alt="" />
+    <React.Fragment>
+      <nav className={colorChange ? "solid-nav" : ""}>
+        <div className="nav-logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="nav-items">
+          <ul>
+            <li className={activeTab === "Home" ? "active" : ""}>
+              <a href="/#" onClick={() => changeActiveTab("Home")}>
+                Home
+              </a>
+            </li>
+            <li className={activeTab === "About" ? "active" : ""}>
+              <a href="/about#" onClick={() => changeActiveTab("About")}>
+                About Us
+              </a>
+            </li>
+            <li className={activeTab === "Service" ? "active" : ""}>
+              <a
+                // href="/#service-section"
+                onClick={() => {
+                  changeActiveTab("Service");
+                  if (dropdown === "none") activateDropwdown();
+                  else deactivateDropdown();
+                }}
+              >
+                Services
+              </a>
+            </li>
+            <li className={activeTab === "Partner" ? "active" : ""}>
+              <a href="/partner#" onClick={() => changeActiveTab("Partner")}>
+                Partner
+              </a>
+            </li>
+            <li className={activeTab === "Contact" ? "active" : ""}>
+              <a
+                href="/#contact-section"
+                onClick={() => changeActiveTab("Contact")}
+              >
+                Contact Us
+              </a>
+            </li>
+          </ul>
+
+          <div />
+        </div>
+      </nav>
+      <div className="extra-class" style={{ display: dropdown }}>
+        <div
+          className={
+            colorChange ? "Service-Dropdown solid-nav" : "Service-Dropdown"
+          }
+        >
+          <a href="/FullTruck#">Full truck Load</a>
+          <a href="/LastMile#">Last Mile Delivery</a>
+        </div>
       </div>
-      <div className="nav-items">
-        <ul>
-          <li className={activeTab === "Home" ? "active" : ""}>
-            <a href="/#" onClick={() => changeActiveTab("Home")}>
-              Home
-            </a>
-          </li>
-          <li className={activeTab === "About" ? "active" : ""}>
-            <a href="/about#" onClick={() => changeActiveTab("About")}>
-              About Us
-            </a>
-          </li>
-          <li className={activeTab === "Service" ? "active" : ""}>
-            <a
-              href="/#service-section"
-              onClick={() => changeActiveTab("Service")}
-            >
-              Services
-            </a>
-          </li>
-          <li className={activeTab === "Partner" ? "active" : ""}>
-            <a href="/partner#" onClick={() => changeActiveTab("Partner")}>
-              Partner
-            </a>
-          </li>
-          <li className={activeTab === "Contact" ? "active" : ""}>
-            <a
-              href="/#contact-section"
-              onClick={() => changeActiveTab("Contact")}
-            >
-              Contact Us
-            </a>
-          </li>
-        </ul>
-        <div />
-      </div>
-    </nav>
+    </React.Fragment>
   );
 };
 
