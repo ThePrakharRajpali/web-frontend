fbbdcfe1ceb39b99c318567898077ceb1c3e0b02
Nikitha Reddy <nikithareddy2309@gmail.com>
2021-12-07 17:54:25 +0000

    Diesel DashBoard 75% Done

--- src/components/Dashboard/Diesel.js
+++ src/components/Dashboard/Diesel.js
@@ -4,6 +4,7 @@ import {DateInput,DatePicker,TimePicker,Calendar,} from "@progress/kendo-react-d
 import * as AiIcons from "react-icons/ai";
 import * as RiIcons from "react-icons/ri";
 import { IconContext } from "react-icons/lib";
+import $ from 'jquery';
 import moment from 'moment';
 import "../../public/css/Dashboard/Dashboard.css";
 import "../../public/css/Dashboard/UserProfile/UserProfile.css";
@@ -44,223 +45,111 @@ const getDates = (startDate, endDate) => {
 }
 
 const info = {
-		Admin1 : {
-			"_id" : "615ae85f288c4050a2c49bc4",
-			"firstName" : "Admin1 Admin1 Admin1 Admin1",
-            "middleName": "Admin1",
-			"lastName": "Admin1",
-			"emailId":"admin1@gmail.com",
-			"contact":"67585938839",
-			"userCode":"NCADMIN0001",
-			"secondaryContact":"98789604034",
-			"address":"Address Street, Address Building, Address Lane, Address City, Address City, Address State, Address Country",
-			"city":"Mumbai",
-			"state":"Maharashtra",
-			"companyName":"Naata Connections",
-			"department":"Management Department",
-			"gst":"78ghj21900",
-			"role":"ADMIN",
-			"active": false,		     "profilePic":"https://raw.githubusercontent.com/Nikitha2309/Private/main/profileDum.jpg?token=APXZ46MZKRP5YYGQAGDCW2DBQZASO",
-			"__v":0,
-			"profilePic":"https://raw.githubusercontent.com/Nikitha2309/Private/main/profileDum.jpg?token=APXZ46MZKRP5YYGQAGDCW2DBQZASO",
+		Diesel1 : {
+			"kmOfVehicle":"1 Km",	"kmOfVehicleImg":"https://raw.githubusercontent.com/Nikitha2309/Private/main/profileDum.jpg?token=APXZ46N7TLYSHWBJRDAWV2DBXDFB4",
+			"pump":"pump 1",
+			"date":"date 1",
+			"liter":1,
+			"dieselRate":100,
+			"totalAmount":1000,
+			"vehicleNumber":"AP00001",
+			"userCode":"NC00001",
+			"paymentMode":"Mode 1",
+			"remarks":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vehicula pellentesque dui in lobortis. Integer efficitur lorem elit, eu rhoncus nisi tristique ut. Aliquam sed ante tortor. Aenean tempor ex a ipsum tempus ullamcorper. Curabitur quis elementum sem. Vivamus ut ultrices ipsum, eget ullamcorper elit. Vivamus aliquet, turpis id vestibulum molestie, orci leo aliquet ligula, vel molestie nunc arcu ac dui. Pellentesque lobortis nunc non erat lobortis semper.",		"billImage":"https://raw.githubusercontent.com/Nikitha2309/Private/main/profileDum.jpg?token=APXZ46N7TLYSHWBJRDAWV2DBXDFB4",
+			
 		},	
-		Admin2 : {
-			"_id" : "615ae85f288c4050a2c49bc4",
-			"firstName" : "Admin2",
-            "middleName": "Admin2",
-			"lastName": "Admin2",
-			"emailId":"admin2@gmail.com",
-			"contact":"67585938839",
-			"secondaryContact":"98789604034",
-			"address":"Address Street, Address Building, Address Lane, Address City, Address City, Address State, Address Country",
-			"city":"Mumbai",
-			"state":"Maharashtra",
-			"companyName":"Naata Connections",
-			"department":"Management Department",
-			"gst":"78ghj21900",
-			"userCode":"NCADMIN0002",
-			"role":"ADMIN",
-			"active": true,		     "profilePic":"https://raw.githubusercontent.com/Nikitha2309/Private/main/profileDum.jpg?token=APXZ46MZKRP5YYGQAGDCW2DBQZASO",
-			"__v":0,
-			"profilePic":"https://raw.githubusercontent.com/Nikitha2309/Private/main/profileDum.jpg?token=APXZ46MZKRP5YYGQAGDCW2DBQZASO",
+		Diesel2 : {
+			"kmOfVehicle":"2 Km",	"kmOfVehicleImg":"https://raw.githubusercontent.com/Nikitha2309/Private/main/profileDum.jpg?token=APXZ46N7TLYSHWBJRDAWV2DBXDFB4",
+			"pump":"pump 2",
+			"date":"date 2",
+			"liter":2,
+			"dieselRate":200,
+			"totalAmount":2000,
+			"vehicleNumber":"AP00002",
+			"userCode":"NC00002",
+			"paymentMode":"Mode 2",
+			"remarks":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vehicula pellentesque dui in lobortis. Integer efficitur lorem elit, eu rhoncus nisi tristique ut. Aliquam sed ante tortor. Aenean tempor ex a ipsum tempus ullamcorper. Curabitur quis elementum sem. Vivamus ut ultrices ipsum, eget ullamcorper elit. Vivamus aliquet, turpis id vestibulum molestie, orci leo aliquet ligula, vel molestie nunc arcu ac dui. Pellentesque lobortis nunc non erat lobortis semper.",		"billImage":"https://raw.githubusercontent.com/Nikitha2309/Private/main/profileDum.jpg?token=APXZ46N7TLYSHWBJRDAWV2DBXDFB4",
+			
 		},
-		Admin3 : {
-			"_id" : "615ae85f288c4050a2c49bc4",
-			"firstName" : "Admin3",
-            "middleName": "Admin3",
-			"lastName": "Admin3",
-			"emailId":"admin3@gmail.com",
-			"contact":"67585938839",
-			"userCode":"NCADMIN0003",
-			"secondaryContact":"98789604034",
-			"address":"Address Street, Address Building, Address Lane, Address City, Address City, Address State, Address Country",
-			"city":"Mumbai",
-			"state":"Maharashtra",
-			"companyName":"Naata Connections",
-			"department":"Management Department",
-			"gst":"78ghj21900",
-			"role":"ADMIN",
-			"active": true,		     "profilePic":"https://raw.githubusercontent.com/Nikitha2309/Private/main/profileDum.jpg?token=APXZ46MZKRP5YYGQAGDCW2DBQZASO",
-			"__v":0,
-			"profilePic":"https://raw.githubusercontent.com/Nikitha2309/Private/main/profileDum.jpg?token=APXZ46MZKRP5YYGQAGDCW2DBQZASO",
-		},	
-		Admin4 : {
-			"_id" : "615ae85f288c4050a2c49bc4",
-			"firstName" : "Admin4",
-            "middleName": "Admin4",
-			"lastName": "Admin4",
-			"emailId":"admin4@gmail.com",
-			"contact":"67585938839",
-			"userCode":"NCADMIN0004",
-			"secondaryContact":"98789604034",
-			"address":"Address Street, Address Building, Address Lane, Address City, Address City, Address State, Address Country",
-			"city":"Mumbai",
-			"state":"Maharashtra",
-			"companyName":"Naata Connections",
-			"department":"Management Department",
-			"gst":"78ghj21900",
-			"role":"ADMIN",
-			"active": true,		     "profilePic":"https://raw.githubusercontent.com/Nikitha2309/Private/main/profileDum.jpg?token=APXZ46MZKRP5YYGQAGDCW2DBQZASO",
-			"__v":0,
-			"profilePic":"https://raw.githubusercontent.com/Nikitha2309/Private/main/profileDum.jpg?token=APXZ46MZKRP5YYGQAGDCW2DBQZASO",
+		Diesel3 : {
+			"kmOfVehicle":"3 Km",	"kmOfVehicleImg":"https://raw.githubusercontent.com/Nikitha2309/Private/main/profileDum.jpg?token=APXZ46N7TLYSHWBJRDAWV2DBXDFB4",
+			"pump":"pump 3",
+			"date":"date 3",
+			"liter":3,
+			"dieselRate":300,
+			"totalAmount":3000,
+			"vehicleNumber":"AP00003",
+			"userCode":"NC00003",
+			"paymentMode":"Mode 3",
+			"remarks":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vehicula pellentesque dui in lobortis. Integer efficitur lorem elit, eu rhoncus nisi tristique ut. Aliquam sed ante tortor. Aenean tempor ex a ipsum tempus ullamcorper. Curabitur quis elementum sem. Vivamus ut ultrices ipsum, eget ullamcorper elit. Vivamus aliquet, turpis id vestibulum molestie, orci leo aliquet ligula, vel molestie nunc arcu ac dui. Pellentesque lobortis nunc non erat lobortis semper.",		"billImage":"https://raw.githubusercontent.com/Nikitha2309/Private/main/profileDum.jpg?token=APXZ46N7TLYSHWBJRDAWV2DBXDFB4",
+			
+		},
+		Diesel4 : {
+			"kmOfVehicle":"4 Km",	"kmOfVehicleImg":"https://raw.githubusercontent.com/Nikitha2309/Private/main/profileDum.jpg?token=APXZ46N7TLYSHWBJRDAWV2DBXDFB4",
+			"pump":"pump 4",
+			"date":"date 4",
+			"liter":4,
+			"dieselRate":400,
+			"totalAmount":4000,
+			"vehicleNumber":"AP00004",
+			"userCode":"NC00004",
+			"paymentMode":"Mode 4",
+			"remarks":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vehicula pellentesque dui in lobortis. Integer efficitur lorem elit, eu rhoncus nisi tristique ut. Aliquam sed ante tortor. Aenean tempor ex a ipsum tempus ullamcorper. Curabitur quis elementum sem. Vivamus ut ultrices ipsum, eget ullamcorper elit. Vivamus aliquet, turpis id vestibulum molestie, orci leo aliquet ligula, vel molestie nunc arcu ac dui. Pellentesque lobortis nunc non erat lobortis semper.",		"billImage":"https://raw.githubusercontent.com/Nikitha2309/Private/main/profileDum.jpg?token=APXZ46N7TLYSHWBJRDAWV2DBXDFB4",
+			
 		},
 }
-
-const Options = (options, handleClick) => {
-	console.log(options);
-	console.log(options["options"].length);
-	if(options["options"].length){
-		return(
-				<div style={{width:'400px',height:'100px',background:'yellow'}}>
-							{options["options"].map((item, index) => {
-							return <button onClick={handleClick} value={item}>
-									 <div>{item}</div>
-									 <div>{item}</div>
-									 <div>{item}</div>
-								   </button>;
-							})}
-				</div>
-			);
-	}
-	else{
-		return (<div></div>);
-	}
-};
 
 class Diesel extends React.Component {
 	
 	
-  constructor(props) {
-    super(props);
-    this.state = {value: '',myOptions:[],users:[],dates:[],searchOption:'searchByDate'};
-	  
-    // this.searchOption = React.createRef();
-    this.handleChangeSearchOption = this.handleChangeSearchOption.bind(this);
-	this.handleChange = this.handleChange.bind(this);
-	this.handleClick = this.handleClick.bind(this);
-	this.handleClickDateChange = this.handleClickDateChange.bind(this);
-	this.changeStartDate = this.changeStartDate.bind(this);
-	this.removeUser = this.removeUser.bind(this);
-	this.handleClear = this.handleClear.bind(this);
-	
-  }
-	
-  removeUser(event,user){
-	 console.log(user);
-	var newUserList = this.state.users;
-	var index = newUserList.indexOf(user);
-	
-	if (index !== -1) {
-	  newUserList.splice(index, 1);
-	}
-	this.setState({users: newUserList});
-  }
-	
-  changeStartDate(event){
-    this.setState({startDate: event.target.value});
-  }
+    constructor(props) {
+		super(props);
+		this.state = {value: '',myOptions:[],diesels:[],dates:[],searchOption:'searchByDate'};
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-	console.log("Enetered input = "+event.target.value);
-	var things = ['Admin1','Admin2','Admin3','Admin4'];
+		this.handleChangeSearchOption = this.handleChangeSearchOption.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+		this.handleClick = this.handleClick.bind(this);
+    }
+
+  	handleChange(event) {
+	// this.setState({value: event.target.value});
+	// console.log("Enetered input = "+event.target.value);
+	var things = ['Diesel1','Diesel2','Diesel3','Diesel4'];
 	var thing1 = things[Math.floor(Math.random()*things.length)];
 	var thing2 = things[Math.floor(Math.random()*things.length)];
+	var thing3 = things[Math.floor(Math.random()*things.length)];
+	var thing4 = things[Math.floor(Math.random()*things.length)];
+	var thing5 = things[Math.floor(Math.random()*things.length)];
 		
-	this.setState({myOptions: [thing1,thing2]});
-	console.log(this.state.myOptions);
-	var results = document.querySelectorAll(".SearchResult");
-	results.forEach((result) => {
-		result.style.display = "flex";
-	});
-  }
+	this.setState({diesels: [info[thing1],info[thing2],info[thing3],info[thing4],info[thing5]]});
+	console.log(this.state.diesels);
+  	}
 	
-  handleChangeSearchOption(event) {
-    this.setState({searchOption: event.target.value});
+  	handleChangeSearchOption(event) {
+	this.setState({searchOption: event.target.value});
 	console.log(this.state.searchOption);
-  }
+  	}
 	
-  handleClick(event) {
-   	console.log("Clicked input = "+event.target.value);
-	this.setState({ users: this.state.users.concat(info[event.target.value]) });
-	this.refs.myInput.value="";
-	var results = document.querySelectorAll(".SearchResult");
-	results.forEach((result) => {
-		result.style.display = "none";
-	});
-	
-  }
-	
-  handleClickDateChange(event){
-	  var start = this.refs.startDate.value;
-	  var end = this.refs.endDate.value;
-	  this.setState({dates: getDates(new Date(start), new Date(end))});   
-  }
-	
-  handleClear(event){
-		this.setState({users: []});
-  }
-
-
-	// getDataFromAPI = () => {
-	// 	console.log("Enetered input = ",this.refs.myInput.value);
-
-	// 	// fetch('http://dummy.restapiexample.com/api/v1/employees').then((response) => {
-	// 	// return response.json()
-	// 	// }).then((res) => {
-	// 	// console.log(res.data)
-	// 	// for (var i = 0; i < res.data.length; i++) {
-	// 	// 	myOptions.push(res.data[i].employee_name)
-	// 	// }
-	// 	// setMyOptions(myOptions)
-	// 	// })
+	handleClick(event) {
+		var arrow = event.target;
+		console.log(arrow);
+		var firstPart = arrow.nextElementSibling;
+		console.log(firstPart);
+		var nextPart = firstPart.nextElementSibling;
+		console.log(nextPart);
 		
-	// 	var things = ['Admin1','Admin2','Admin3','Admin4'];
-	// 	var thing1 = things[Math.floor(Math.random()*things.length)];
-	// 	var thing2 = things[Math.floor(Math.random()*things.length)];
+		while(nextPart){
+			nextPart.slideToggle("fast","linear");
+			nextPart = nextPart.nextElementSibling;	
+			console.log(nextPart);
+		}
 		
-	// 	setMyOptions(['Admin1','Admin2']);
-	// }
-	
-	// getDataFromLi = (index) => {
-	// 	console.log("Options Fetched from User");
-
-	// 	// fetch('http://dummy.restapiexample.com/api/v1/employees').then((response) => {
-	// 	// return response.json()
-	// 	// }).then((res) => {
-	// 	// console.log(res.data)
-	// 	// for (var i = 0; i < res.data.length; i++) {
-	// 	// 	myOptions.push(res.data[i].employee_name)
-	// 	// }
-	// 	// setMyOptions(myOptions)
-	// 	// })
-		
-	// 	setUser(info.Admin1);
-	// 	console.log(user.firstName);
-	// }
+		// if(event.target.style.transform=="rotate(-180deg)")  											event.target.style.transform="rotate(0deg)";
+		// else 
+		// 	event.target.style.transform="rotate(-180deg)";
+	}
 	
  render() {
-	 
 	 
 	return (
 		<div className="Dashboard">
@@ -285,6 +174,8 @@ class Diesel extends React.Component {
 			 
 			</div>
 			
+			<div>
+			
 			{ 
 				this.state.searchOption == "searchByDate" ? 
 				 (
@@ -304,7 +195,7 @@ class Diesel extends React.Component {
 				this.state.searchOption == "searchByModeOfPayment" ?
 				(
 					<div>
-						<select className="SearchBarInput">
+						<select className="SearchBarInput" onChange={this.handleChange} >
 							<option value="phonePe">PhonePe</option>
 							<option value="cash">Cash</option>
 						</select>
@@ -327,99 +218,63 @@ class Diesel extends React.Component {
 				)
 			}
 			
-			
-			
-			
-			<div style={{height:'auto'}} ref="SearchResults">
-				{this.state.myOptions.map((item, index) => {
+		
+			</div>
+				
+			{this.state.diesels.map((diesel, index) => {
 				return (
-					<div className="SearchResult">
-						<button className="SearchResultItem" style={{width:'33%'}} onClick={this.handleClick} value={item}>{index+" "+item + " " + item + " " + item}</button>
-						<button className="SearchResultItem" style={{width:'33%'}} onClick={this.handleClick} value={item}>{index+" "+item}</button>
-						<button className="SearchResultItem" style={{width:'33%'}} onClick={this.handleClick} value={item}>{index+" "+item}</button>
+					<div className="Diesel_Cards">
+						<div className="Form_Field_Container">
+
+							<div className="DieselCardDropDown" onClick={this.handleClick}>
+								<IconContext.Provider className="SearchBarDropDown" value={{ color: "#7E8080", size:'1.73vw' }}>
+									<RiIcons.RiArrowDownSFill />
+								</IconContext.Provider> 
+							</div>
+							
+							<div className="Form_Fields">
+
+								<div className="Form_field">
+									<label className="Form_field_label">Vehicle Number</label>
+									<input required className="Form_field_input" style={{width:'20vw',height:'2.6vw'}} disabled={true} value={diesel["vehicleNumber"]}/>
+								</div>
+
+								<div className="Form_field">
+									<label className="Form_field_label">User Code</label>
+									<input className="Form_field_input" style={{width:'20vw',height:'2.6vw'}} disabled={true} value={diesel["userCode"]} />
+								</div>
+
+								<div className="Form_field">
+									<label className="Form_field_label">Total Amount</label>
+									<input required className="Form_field_input" style={{width:'20vw',height:'2.6vw'}} disabled={true} value={diesel["totalAmount"]}/>
+								</div>
+
+							</div>
+							
+							<div className="Form_Fields" style={{display:"none"}}>
+
+								<div className="Form_field">
+									<label className="Form_field_label">Vehicle Number</label>
+									<input required className="Form_field_input" style={{width:'20vw',height:'2.6vw'}} disabled={true} value={diesel["vehicleNumber"]}/>
+								</div>
+
+								<div className="Form_field">
+									<label className="Form_field_label">User Code</label>
+									<input className="Form_field_input" style={{width:'20vw',height:'2.6vw'}} disabled={true} value={diesel["userCode"]} />
+								</div>
+
+								<div className="Form_field">
+									<label className="Form_field_label">Total Amount</label>
+									<input required className="Form_field_input" style={{width:'20vw',height:'2.6vw'}} disabled={true} value={diesel["totalAmount"]}/>
+								</div>
+
+							</div>
+
+						</div>
 					</div>
 					);
 				})}
-			</div>
-			
-			<div className="DateRangeRow">
-				<div className="DateRangeTitle">Date Range</div>
-				<div className="dateInput">
-					<label className="dateInputLabel">Start Date</label>
-					<input type="date" className="dateInputInput" ref="startDate" disabled={false} onChange={this.handleClickDateChange}></input>
-				</div>
-				<div className="dateInput">
-					<label className="dateInputLabel">End Date</label>
-					<input type="date" className="dateInputInput" ref="endDate" disabled={false} onChange={this.handleClickDateChange} ></input>
-				</div>
-				<button className="dateButton" onClick={this.handleClickDateChange}></button>
-
-			</div>
-			
-			<div className="clearAttendanceRow">
-			      <button className="clearAttendanceButton" onClick={this.handleClear}>Clear Dashboard</button>
-			</div>
-			
-			<div className="AttendanceTableDiv">
-				<table className="AttendanceTable">
-				<tr className="AttendanceRow1">
-					<td className="AttendanceCell"></td>
-					{this.state.dates.map((item) => {
-				return (
-					<td className="AttendanceCell">{item}</td>
-					);
-				})}
-				</tr>
-				
-				{this.state.users.map((item,index) => {
-				return (
-					<tr className={index%2 ? "AttendanceRow1": "AttendanceRow2"}>
-						<td className="AttendanceCell">
-							{item.firstName}
-							<button className="AttendanceUserDelete" onClick={()=>{
-																					console.log(item);
-																					var newUserList = this.state.users;
-																					var index = newUserList.indexOf(item);
-
-																					if (index !== -1) {
-																					  newUserList.splice(index, 1);
-																					}
-																					this.setState({users: newUserList});}}                                              type='button'>
-								<IconContext.Provider value={{ color: "#E5584F", size:'1vw' }}>
-									<RiIcons.RiCloseCircleLine />
-								</IconContext.Provider> 
-							</button>
-							<button className="AttendanceUserDonwload" >
-								<IconContext.Provider value={{ color: "#52B788", size:'1vw' }}>
-									<RiIcons.RiDownload2Line />
-								</IconContext.Provider> 
-							</button>
-						</td>
-						{this.state.dates.map((item) => {return (<td className="AttendanceCell">
-																	 P
-																	 &nbsp;
-																	 &nbsp;
-																	<IconContext.Provider className="SearchBarIcon" value={{ color: "#F3752B", size:'1.33vw' }}>
-																	 <AiIcons.AiOutlineClockCircle />
-																	&nbsp;
-																	
-																    <RiIcons.RiMapPin2Line />
-																		
-																	 </IconContext.Provider> 
-																 </td>);})}
-					</tr>
-	             );
-				  })}
-				</table>
-			</div>
-			
-			<button className="AttendanceDownload">
-				<IconContext.Provider value={{ color: "#ffffff", size:'1vw' }}>
-					<RiIcons.RiFileDownloadLine />
-			    </IconContext.Provider> 
-				&nbsp; Download
-			</button>
-			
+	
 		</div>
 	);
  }
