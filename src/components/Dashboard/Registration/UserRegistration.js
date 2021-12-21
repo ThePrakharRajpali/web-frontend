9d45b55bed9d3de400b331065e3d1e384a9e5717
Nikitha Reddy <nikithareddy2309@gmail.com>
2021-12-21 13:43:11 +0000

    View Profiles Integration 40% Done

--- src/components/Dashboard/Registration/UserRegistration.js
+++ src/components/Dashboard/Registration/UserRegistration.js
@@ -18,8 +18,8 @@ class UserRegistration extends React.Component {
   handleRegister(event) {
 	  
 		const re_email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		// const re_phoneNum = /^\d{10}$/; 
-	    const re_phoneNum = /^\+?([0-9]{2})\)?[-]?([0-9]{3})[ ]?([0-9]{3})?[-]?([0-9]{4})$/;
+		const re_phoneNum = /^\d{10}$/; 
+	    // const re_phoneNum = /^\+?([0-9]{2})\)?[-]?([0-9]{3})[ ]?([0-9]{3})?[-]?([0-9]{4})$/;
 
 		var firstName =  this.refs.firstName.value;
 		var middleName = this.refs.middleName.value;
