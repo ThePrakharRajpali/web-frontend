c89156b17d0e0be958290128ebd886ba37531be7
Prakhar Rajpali <rajpaliprakhar23@gmail.com>
2021-12-28 21:24:15 +0530

    Pages responsiveness done

--- src/components/About/Landing.js
+++ src/components/About/Landing.js
@@ -1,7 +1,60 @@
 import React from "react";
+import "../../public/css/About/Landing.css";
+import grid from "../../public/photos/Blue_grid.svg";
+import bg from "../../public/photos/Background_truck.png";
 
 const Landing = () => {
-  return <div></div>;
+  return (
+    <div className="about-landing">
+      <div className="about-background">
+        <img src={bg} alt="" id="background-about" />
+      </div>
+
+      <div className="about-landing-content">
+        <div className="about-landing-header">
+          <img src={grid} alt="" />
+          <p>About Us</p>
+        </div>
+
+        <div className="about-landing-main">
+          <p>
+            Since its inception in 2018, Naata Connection has become a leading
+            logistics and supply chain services company in Madhya Pradesh. Our
+            vision is to become the operating system for last-mile deliveries in
+            India.
+          </p>
+
+          <br />
+          <br />
+
+          <p className="about-landing-sub-header">
+            Market Leader In Supply Chain & Logistics
+          </p>
+
+          <br />
+
+          <p>
+            Naata Connection began its journey in 2018 with a mission of
+            delivering logistics excellence to its customers and ensuring their
+            success. Today, The company has firmly entrenched itself as the know
+            service provider of the Last-Mile Delivery industry in India.
+            <br />
+            <br /> Naata Connection offers a wide range of innovative supply
+            chain services including Last-Mile Delivery, Express Movement. The
+            company provides logistics services for 4 different business
+            verticals ranging from E-commerce, Engineering & Electrical
+            Hardware, FMCG & Consumer Electronics, and Institutional.
+            <br />
+            <br /> We offer cutting-edge logistics solutions to our customers,
+            enabling them to focus on their core competencies. The company adds
+            maximum value to businesses at every level, right from providing
+            world-class logistics support to ensuring time-definite deliveries
+            of goods anywhere across India.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
--- src/components/Dashboard/ServiceRequests/All.js
+++ src/components/Dashboard/ServiceRequests/All.js
@@ -1,4 +1,6 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
+
 import "../../../public/css/Dashboard/Dashboard.css";
 import "../../../public/css/Dashboard/ServiceRequest/all.css";
 import threedot from "../../../public/photos/threedot.svg";
@@ -15,20 +17,20 @@ const Header = () => {
   );
 };
 
-const Row = () => {
+const Row = ({ data }) => {
   return (
     <Fragment>
       <ul className="table-row">
-        {/* <li>{data.requestCode}</li>
+        <li>{data.requestCode}</li>
         <li>{data.status}</li>
         <li>{data.customer}</li>
         <li>{data.startDate}</li>
-        <li>{data.startTime}</li> */}
-        <li>ID12345678</li>
+        <li>{data.startTime}</li>
+        {/* <li>ID12345678</li>
         <li>Approved</li>
         <li>Customer</li>
         <li>24-10-2021</li>
-        <li>5:00 PM</li>
+        <li>5:00 PM</li> */}
         <li className="edit">
           <img src={pencil} alt="" />
           <img src={threedot} alt="" />
@@ -39,6 +41,7 @@ const Row = () => {
 };
 
 const List = ({ list }) => {
+  // console.log(list);
   return (
     <div className="table">
       <ul className="table-header">
@@ -50,30 +53,37 @@ const List = ({ list }) => {
         <li>Edit</li>
       </ul>
       <div className="request-rows">
-        <Row />
-        <Row />
-        <Row />
-        <Row />
-        <Row />
-        <Row />
-        <Row />
-        <Row />
-        <Row />
-        <Row />
-        <Row />
-        <Row />
-        <Row />
-        <Row />
-        <Row />
-        <Row />
-        <Row />
+        {list.map((response, index) => {
+          if (response.toShow) {
+            return <Row key={index} data={response} />;
+          }
+        })}
       </div>
     </div>
   );
 };
 
-const SearchBar = () => {
+const SearchBar = ({ requests }) => {
   const [search, setSearch] = useState("");
+
+  // const [list, setList] = useState(
+  //   requests.data.data.map((request, index) => {
+  //     return { ...request, toShow: true };
+  //   })
+  // );
+
+  // console.log(list);
+  // useEffect(() => {
+  //   const copyList = list.map((request, index) => {
+  //     // let show = request.requestCode.toString().contains(search);
+  //     // let show1 = request.customer.toString().contains(search);
+  //     return {
+  //       ...request,
+  //       toShow: false,
+  //     };
+  //   });
+  //   setList(copyList);
+  // }, [search]);
 
   return (
     <div>
@@ -90,16 +100,66 @@ const SearchBar = () => {
         </button>
       </div>
 
-      <List list={[]} />
+      {/* <List
+        list={requests.data.data.map((request, index) => {
+          return { ...request, toShow: true };
+        })}
+        search={search}
+      /> */}
+
+      <div className="table">
+        <ul className="table-header">
+          <li>Request ID</li>
+          <li>Status</li>
+          <li>Customer</li>
+          <li>Date</li>
+          <li>Time</li>
+          <li>Edit</li>
+        </ul>
+        <div className="request-rows">
+          {requests.data.data.map((response, index) => {
+            if (response.toShow) {
+              return <Row key={index} data={response} />;
+            }
+          })}
+        </div>
+      </div>
     </div>
   );
 };
 
 const ServiceRequests_All = () => {
+  const [requests, setRequests] = useState({});
+  const [loading, setLoading] = useState(true);
+  const data = "";
+  const apiConfig = {
+    method: "get",
+    url: "https://www.naataconnection.com/api/serviceRequest/",
+    headers: {},
+    data: data,
+  };
+  const getData = (apiConfig) => {
+    return axios(apiConfig)
+      .then((response) => {
+        // console.log(JSON.stringify(response.data));
+        setRequests(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    getData(apiConfig);
+  }, []);
+
   return (
     <div className="Dashboard">
       <Header />
-      <SearchBar />
+      {loading ? <div> Loading</div> : <SearchBar requests={requests} />}
     </div>
   );
 };
